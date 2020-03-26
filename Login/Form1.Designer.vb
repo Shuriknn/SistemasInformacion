9aba263fcc5bea965d2441bbe75ca5c3d7f7c5bd
Shuriknn <62680335+Shuriknn@users.noreply.github.com>
2020-03-25 19:47:15 -0600

    Add files via upload

---
+++ Login/Form1.Designer.vb
@@ -0,0 +1,252 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape2 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.txtuser = New System.Windows.Forms.TextBox()
+        Me.txtpass = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnlogin = New System.Windows.Forms.Button()
+        Me.btnminimizar = New System.Windows.Forms.PictureBox()
+        Me.btncerrar = New System.Windows.Forms.PictureBox()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btnminimizar, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.btncerrar, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(128, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(250, 330)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(250, 29)
+        Me.Panel2.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Monotype Corsiva", 24.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(65, 250)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(114, 39)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "FRIDA"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Monotype Corsiva", 18.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(50, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(144, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "BALCÓN DE"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox3.Image = Global.Login.My.Resources.Resources.Imagen1
+        Me.PictureBox3.Location = New System.Drawing.Point(12, 55)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(223, 220)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox3.TabIndex = 0
+        Me.PictureBox3.TabStop = False
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape2, Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(780, 330)
+        Me.ShapeContainer1.TabIndex = 1
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape2
+        '
+        Me.LineShape2.BorderColor = System.Drawing.Color.DimGray
+        Me.LineShape2.Enabled = False
+        Me.LineShape2.Name = "LineShape2"
+        Me.LineShape2.X1 = 310
+        Me.LineShape2.X2 = 720
+        Me.LineShape2.Y1 = 171
+        Me.LineShape2.Y2 = 171
+        '
+        'LineShape1
+        '
+        Me.LineShape1.BorderColor = System.Drawing.Color.DimGray
+        Me.LineShape1.Enabled = False
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 310
+        Me.LineShape1.X2 = 720
+        Me.LineShape1.Y1 = 91
+        Me.LineShape1.Y2 = 91
+        '
+        'txtuser
+        '
+        Me.txtuser.BackColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer))
+        Me.txtuser.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtuser.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtuser.ForeColor = System.Drawing.Color.DimGray
+        Me.txtuser.HideSelection = False
+        Me.txtuser.Location = New System.Drawing.Point(310, 71)
+        Me.txtuser.Name = "txtuser"
+        Me.txtuser.Size = New System.Drawing.Size(411, 20)
+        Me.txtuser.TabIndex = 2
+        Me.txtuser.Text = "Usuario"
+        '
+        'txtpass
+        '
+        Me.txtpass.BackColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer))
+        Me.txtpass.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtpass.Font = New System.Drawing.Font("Century Gothic", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtpass.ForeColor = System.Drawing.Color.DimGray
+        Me.txtpass.Location = New System.Drawing.Point(310, 151)
+        Me.txtpass.Name = "txtpass"
+        Me.txtpass.Size = New System.Drawing.Size(411, 20)
+        Me.txtpass.TabIndex = 3
+        Me.txtpass.Text = "Contraseña"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Century Gothic", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DimGray
+        Me.Label1.Location = New System.Drawing.Point(461, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(100, 33)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "LOGIN"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'btnlogin
+        '
+        Me.btnlogin.BackColor = System.Drawing.Color.FromArgb(CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer), CType(CType(40, Byte), Integer))
+        Me.btnlogin.FlatAppearance.BorderSize = 0
+        Me.btnlogin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer), CType(CType(28, Byte), Integer))
+        Me.btnlogin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer), CType(CType(64, Byte), Integer))
+        Me.btnlogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnlogin.ForeColor = System.Drawing.Color.LightGray
+        Me.btnlogin.Location = New System.Drawing.Point(310, 250)
+        Me.btnlogin.Name = "btnlogin"
+        Me.btnlogin.Size = New System.Drawing.Size(410, 40)
+        Me.btnlogin.TabIndex = 1
+        Me.btnlogin.Text = "ENTRAR"
+        Me.btnlogin.UseVisualStyleBackColor = False
+        '
+        'btnminimizar
+        '
+        Me.btnminimizar.Image = Global.Login.My.Resources.Resources.window_minimize
+        Me.btnminimizar.Location = New System.Drawing.Point(709, 3)
+        Me.btnminimizar.Name = "btnminimizar"
+        Me.btnminimizar.Size = New System.Drawing.Size(32, 26)
+        Me.btnminimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btnminimizar.TabIndex = 7
+        Me.btnminimizar.TabStop = False
+        '
+        'btncerrar
+        '
+        Me.btncerrar.Image = Global.Login.My.Resources.Resources.close_thick
+        Me.btncerrar.Location = New System.Drawing.Point(747, 3)
+        Me.btncerrar.Name = "btncerrar"
+        Me.btncerrar.Size = New System.Drawing.Size(30, 26)
+        Me.btncerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.btncerrar.TabIndex = 6
+        Me.btncerrar.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.Location = New System.Drawing.Point(247, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(533, 29)
+        Me.Panel3.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer), CType(CType(15, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(780, 330)
+        Me.Controls.Add(Me.btnminimizar)
+        Me.Controls.Add(Me.btncerrar)
+        Me.Controls.Add(Me.btnlogin)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtpass)
+        Me.Controls.Add(Me.txtuser)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Name = "Form1"
+        Me.Opacity = 0.93R
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btnminimizar, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.btncerrar, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape2 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents txtuser As System.Windows.Forms.TextBox
+    Friend WithEvents txtpass As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents btnlogin As System.Windows.Forms.Button
+    Friend WithEvents btncerrar As System.Windows.Forms.PictureBox
+    Friend WithEvents btnminimizar As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+End Class
---
+++ Login/Form1.vb
@@ -0,0 +1,86 @@
+﻿Public Class Form1
+
+    Private mouseDowns As Boolean
+    Private lastLocation As Point
+
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles txtpass.TextChanged
+
+    End Sub
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles btncerrar.Click
+        Application.Exit()
+
+    End Sub
+
+    Private Sub txtuser_Enter(sender As Object, e As EventArgs) Handles txtuser.Enter
+        If (txtuser.Text = "Usuario") Then
+            txtuser.Clear()
+            txtuser.ForeColor = Color.LightGray
+        End If
+    End Sub
+
+    Private Sub txtuser_Leave(sender As Object, e As EventArgs) Handles txtuser.Leave
+        If (txtuser.Text = "") Then
+            txtuser.Text = "Usuario"
+            txtuser.ForeColor = Color.DimGray
+        End If
+    End Sub
+
+    Private Sub txtpass_Enter(sender As Object, e As EventArgs) Handles txtpass.Enter
+        If (txtpass.Text = "Contraseña") Then
+            txtpass.Clear()
+            txtpass.ForeColor = Color.LightGray
+            txtpass.UseSystemPasswordChar = True
+        End If
+    End Sub
+
+    Private Sub txtpass_Leave(sender As Object, e As EventArgs) Handles txtpass.Leave
+        If (txtpass.Text = "") Then
+            txtpass.Text = "Contraseña"
+            txtpass.ForeColor = Color.DimGray
+            txtpass.UseSystemPasswordChar = False
+        End If
+    End Sub
+
+    Private Sub btnminimizar_Click(sender As Object, e As EventArgs) Handles btnminimizar.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+    Private Sub Panel2_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel2.MouseDown
+        mouseDowns = True
+        lastLocation = e.Location
+    End Sub
+
+    Private Sub Panel2_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel2.MouseMove
+        If mouseDowns Then
+            Me.Location = New Point(
+                (Me.Location.X - lastLocation.X) + e.X, (Me.Location.Y - lastLocation.Y) + e.Y)
+
+            Me.Update()
+        End If
+    End Sub
+
+    Private Sub Panel2_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel2.MouseUp
+        mouseDowns = False
+    End Sub
+
+    Private Sub Panel3_MouseDown(sender As Object, e As MouseEventArgs) Handles Panel3.MouseDown
+        mouseDowns = True
+        lastLocation = e.Location
+    End Sub
+
+    Private Sub Panel3_MouseMove(sender As Object, e As MouseEventArgs) Handles Panel3.MouseMove
+        If mouseDowns Then
+            Me.Location = New Point(
+                (Me.Location.X - lastLocation.X) + e.X, (Me.Location.Y - lastLocation.Y) + e.Y)
+
+            Me.Update()
+        End If
+    End Sub
+
+    Private Sub Panel3_MouseUp(sender As Object, e As MouseEventArgs) Handles Panel3.MouseUp
+        mouseDowns = False
+    End Sub
+End Class
